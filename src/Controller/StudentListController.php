f826a205fb304b8b92656703130a0440113cb4e9
Justina Jankauskaite <jjan.juste@gmail.com>
2018-04-13 22:05:30 +0300

    Add StudentList Controller and template

---
+++ src/Controller/StudentListController.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ruta
+ * Date: 18.4.13
+ * Time: 21.23
+ */
+
+namespace App\Controller;
+
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class StudentListController extends Controller
+{
+
+    const STUDENT_DATA = 'data.json';
+
+    /**
+     * @Route("/students", name="student_list")
+     */
+    public function students()
+    {
+        $dataJson = file_get_contents(self::STUDENT_DATA);
+        $data = json_decode($dataJson, true);
+
+        return $this->render('students/studentlist.html.twig', [
+            'studentList' => $data
+        ]);
+    }
+}
