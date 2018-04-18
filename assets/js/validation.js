c0bad11c7ea12d24ee35052e4fdcab0f4485399b
Justina Jankauskaitė <jjan.juste@gmail.com>
2018-04-18 14:42:36 +0300

    finish up homework

--- assets/js/validation.js
+++ assets/js/validation.js
@@ -1,2 +1,23 @@
 const axios = require('axios');
 
+
+function postData (id)
+{
+    let validationPath = document.getElementById(this.id+'-validation');
+
+    axios.post(validationPath.dataset.path, {input: this.value})
+        .then(function(response) {
+            if (response.data.valid) {
+                validationPath.innerText = ":)";
+            } else {
+                validationPath.innerText = ":(";
+            }
+        })
+        .catch(function (error) {
+            validationPath.innerText = 'Error: ' + error;
+        });
+}
+
+
+document.getElementById('name').onkeyup = postData;
+document.getElementById('team').onkeyup = postData;
--- src/Controller/StudentController.php
+++ src/Controller/StudentController.php
@@ -5,19 +5,64 @@ namespace App\Controller;
 
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 
 
 class StudentController extends Controller
 {
+    const STUDENT_DATA = 'data.json';
+
+    /**
+     * @Route("/people")
+     */
+    public function inputAction()
+    {
+        return $this->render('input/index.html.twig');
+    }
+
     /**
      * @Route("/validation/{key}", name="validation")
-     * @Method("POST")
+     * @Method({"POST"})
      */
     public function validationAction(Request $request, $key)
     {
-        $data = json_decode($request, true)['input'];
+        try {
+            $data = json_decode($request->getContent(), true)['input'];
+        } catch (\Exception $e) {
+            return new JsonResponse(['error' => 'Invalid Method'], Response::HTTP_BAD_REQUEST);
+        }
+
+        $validationData = $this->getData();
+
+        switch ($key) {
+            case 'name':
+            case 'team':
+                return new JsonResponse(['valid' => in_array(strtolower($data), $validationData[$key])]);
+        }
+
+        return new JsonResponse(['error' => 'Invalid method'], Response::HTTP_BAD_REQUEST);
+    }
+
+    private function getData()
+    {
+        $dataJson = file_get_contents(self::STUDENT_DATA);
+        $data = json_decode($dataJson, true);
+        $teams = [];
+        $students = [];
+        foreach ($data as $teamName => $teamData) {
+            $teams[] = strtolower($teamName);
+            foreach($teamData['members'] as $student) {
+                $students[] = strtolower($student);
+            }
+        }
+
+        return [
+            'team' => $teams,
+            'name' => $students
+        ];
     }
 }
